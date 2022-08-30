9bdf48f1050b70c875f762dc822639c90b9c66c2
Chan-Hung <hungkevin19122001@gamil.com>
2022-08-30 10:46:55 +0700

    Doctype field dependency

---
+++ estate_app/estate_app/doctype/property/property.py
@@ -0,0 +1,16 @@
+# Copyright (c) 2022, Hung DChan and contributors
+# For license information, please see license.txt
+
+import frappe
+from frappe.model.document import Document
+
+class Property(Document):
+	#validate
+	# def validate(self):
+	# 	if(self.property_type == "Flat"):
+	# 		for amenity in self.amenities:
+	# 			if(amenity.amenity == "Outdoor Kitchen"):
+	# 				frappe.throw((f'Property of type <b>Flat</b> should not have amenity {amenity.amenity}'))
+
+	def after_insert(self):
+		frappe.msgprint((f'Document {self.name} inserted successfully'))
